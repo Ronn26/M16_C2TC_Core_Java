589cbbf5fcb8e17140cc6a1128da5141ec981d58
Roshan <Roshan@LAPTOP-F2F4UUML>
2022-04-19 16:13:54 +0530

    First commit

---
+++ Day9_Interface_WrapperClasses/src/com/indiabix/Wrapper/Example1.java
@@ -0,0 +1,14 @@
+package com.indiabix.Wrapper;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		float x=20.5f;
+		Float y=x;
+		Float obj=new Float(15.0f);
+		System.out.println(x+" "+y+" "+obj);
+
+	}
+
+}
---
+++ Day9_Interface_WrapperClasses/src/com/indiabix/Wrapper/Example2.java
@@ -0,0 +1,14 @@
+package com.indiabix.Wrapper;
+
+public class Example2 
+{
+
+	public static void main(String[] args) 
+	{
+		Character x='s';
+		char y=x;
+		System.out.println(x+" "+y);
+
+	}
+
+}
