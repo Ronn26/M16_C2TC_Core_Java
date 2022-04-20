6549aaf36a456a98635502c9d43c875dac95d7f2
Roshan <Roshan@LAPTOP-F2F4UUML>
2022-04-20 17:29:51 +0530

    first commit

---
+++ Day10_Array/src/com/indiabix/array/Example2.java
@@ -0,0 +1,16 @@
+package com.indiabix.array;
+
+public class Example2 
+{
+
+	public static void main(String[] args) 
+	{
+		int arr[]= {11,22,33,44,55};
+		for (int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]+" ");
+		}
+
+	}
+
+}
