cc9c2bdb18b31a70187d218b744e151ef8de0656
Roshan <Roshan@LAPTOP-F2F4UUML>
2022-04-27 00:12:10 +0530

    first commit

--- Day11_Exception/src/com/indiabix/exception/Program3.java
+++ Day11_Exception/src/com/indiabix/exception/Program3.java
@@ -1,29 +1,26 @@
 package com.indiabix.exception;
 
-public class Program3 
+public class Program3
 {
-    public static void main(String args[])
-    {
-    	System.out.println(Program3.myMethod());
-    }
-    public static void myMethod()
-    {
-    	try
-    	{
-    		int x=63;
-    		int y=0;
-    		int z=x/y;
-    		System.out.println("Inside try Block");
-    		System.exit(C);
-    	}
-    	catch(Exception exp)
-    	{
-    		System.out.println(exp);
-    	}
-    	finally
-    	{
-    		System.out.println("Java finally Block");
-    		return C;
-    	}
-    }
-}
+	   public static void main(String args[])
+	   {
+	      System.out.println(Program3.myMethod());  
+	   }
+	   public static int myMethod(){
+	       try {
+	                int x = 63;
+	                int y = 0;
+	                int z=x/y;
+	                System.out.println("Inside try block");
+	                System.exit(0);
+	        }
+	        catch (Exception exp){
+	            System.out.println(exp);
+	        }
+	       
+	        finally{
+	            System.out.println("Java finally block");
+	            return 0;
+	        }
+	   }
+	}
