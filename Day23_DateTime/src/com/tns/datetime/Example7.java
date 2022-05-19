561cd64a17e95ac432ae1391eaf1581ab57f9170
Roshan <Roshan@LAPTOP-F2F4UUML>
2022-05-19 13:09:50 +0530

    first commit

---
+++ Day23_DateTime/src/com/tns/datetime/Example7.java
@@ -0,0 +1,14 @@
+package com.tns.datetime;
+
+import java.time.Instant;
+
+public class Example7 {
+
+	@SuppressWarnings("static-access")
+	public static void main(String[] args) {
+		Instant a=Instant.EPOCH.MAX;
+		System.out.println(a);
+
+	}
+
+}
